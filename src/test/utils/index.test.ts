71e10386aa59e7fd185b2739af187327a5645331
Jimmy Thomson <jimmy@gradientedge.com>
2021-04-12 23:28:02 +0100

    fix: Detect node before attempting to set the User-Agent header.

--- src/test/utils/index.test.ts
+++ src/test/utils/index.test.ts
@@ -1,15 +1,15 @@
 import { buildUserAgent } from '../../lib/utils'
 
 describe('buildUserAgent', () => {
-  it('should use only the package name and version when no system identifier is passed in', () => {
-    expect(buildUserAgent()).toBe('commercetools-utils/0.0.0-development')
+  it('should use only the package name when no system identifier is passed in', () => {
+    expect(buildUserAgent()).toBe('@gradientedge/commercetools-utils')
   })
 
   it('should append the system identifier when passed in', () => {
-    expect(buildUserAgent('my-system')).toBe('commercetools-utils/0.0.0-development (my-system)')
+    expect(buildUserAgent('my-system')).toBe('@gradientedge/commercetools-utils (my-system)')
   })
 
   it('should ignore a null value for the system identifier', () => {
-    expect(buildUserAgent((null as unknown) as string)).toBe('commercetools-utils/0.0.0-development')
+    expect(buildUserAgent((null as unknown) as string)).toBe('@gradientedge/commercetools-utils')
   })
 })
