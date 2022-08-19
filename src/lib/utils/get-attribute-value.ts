74c9dce392f91e291bd68002e5bd9ecd22fee55f
Jimmy Thomson <jimmy@gradientedge.com>
2022-08-19 17:44:34 +0100

    feat(util): add getAttributeValue function
    Utility function for retrieving an attribute value out of the list of attributes

---
+++ src/lib/utils/get-attribute-value.ts
@@ -0,0 +1,27 @@
+import { Attribute } from '../models'
+
+/**
+ * Options interface for {@see getAttributeValue}
+ */
+
+export interface GetAttributeValueParams {
+  /** Attribute list */
+  attributes: Attribute[] | null | undefined
+  /** Name of the attribute that you want to retrieve the value of */
+  name: string
+}
+
+/**
+ * Retrieve the value of the attribute for the given attribute name
+ *
+ * Returns `null` if no attribute is found
+ */
+export function getAttributeValue<T = any>(options: GetAttributeValueParams): T | null {
+  if (options?.attributes?.length) {
+    const attribute = options.attributes?.find((attribute) => attribute.name === options.name)
+    if (attribute) {
+      return attribute.value
+    }
+  }
+  return null
+}
---
+++ src/test/utils/get-attribute-value.test.ts
@@ -0,0 +1,57 @@
+import { getAttributeValue } from '../../lib/utils/get-attribute-value'
+
+describe('getAttributeValue', () => {
+  it('should return null if the attribute list is undefined', () => {
+    expect(getAttributeValue({ attributes: undefined, name: 'test' })).toBeNull()
+  })
+
+  it('should return null if the attribute list is null', () => {
+    expect(getAttributeValue({ attributes: null, name: 'test' })).toBeNull()
+  })
+
+  it('should return null if the attribute list is empty', () => {
+    expect(getAttributeValue({ attributes: [], name: 'test' })).toBeNull()
+  })
+
+  it('should return null if the attribute does not exist in the attribute list', () => {
+    const result = getAttributeValue({
+      attributes: [
+        {
+          name: 'example1',
+          value: 'test',
+        },
+      ],
+      name: 'test',
+    })
+
+    expect(result).toBeNull()
+  })
+
+  it('should return the attribute string value if the attribute exists in the attribute list', () => {
+    const result = getAttributeValue({
+      attributes: [
+        {
+          name: 'test',
+          value: 'my-value',
+        },
+      ],
+      name: 'test',
+    })
+
+    expect(result).toBe('my-value')
+  })
+
+  it('should return the attribute array value if the attribute exists in the attribute list', () => {
+    const result = getAttributeValue({
+      attributes: [
+        {
+          name: 'test',
+          value: ['my-value-1', 'my-value-2'],
+        },
+      ],
+      name: 'test',
+    })
+
+    expect(result).toEqual(['my-value-1', 'my-value-2'])
+  })
+})
