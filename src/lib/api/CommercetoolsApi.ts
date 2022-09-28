ca32c02b4ba7a5f458c9c4a88b0a83887e7a618a
Jimmy Thomson <jimmy@gradientedge.com>
2022-09-28 17:27:21 +0200

    feat: send body data to log function

--- src/lib/api/CommercetoolsApi.ts
+++ src/lib/api/CommercetoolsApi.ts
@@ -322,6 +322,7 @@ export class CommercetoolsApi {
             method: config.method as string,
             params: config.params,
             headers: config.headers,
+            data: config.data,
           })
         }
         return config
