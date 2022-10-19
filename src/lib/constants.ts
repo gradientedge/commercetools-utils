ddef1b6195520eb8287b0b41027a3d6168559503
Jimmy Thomson <jimmy@gradientedge.com>
2022-10-19 18:23:49 +0100

    feat: expose constant defining max items per query

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,8 @@
  * this to `axios` when making the request to commercetools.
  */
 export const DEFAULT_REQUEST_TIMEOUT_MS = 5000
+
+/**
+ * The maximum number of items that can be returned in a query
+ */
+export const MAX_ITEMS_PER_QUERY = 500
