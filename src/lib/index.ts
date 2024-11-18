32b51120b2301b396270757157aafadb8397cccd
Manuel Moranchel <manu.moranchel@gradientedge.com>
2024-11-18 15:00:01 +0000

    feat: MCA-623 wrap the commercetools/platform-sdk to use commercetools models directly

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,6 @@ export * from './auth/index.js'
 export * from './api/index.js'
 export * from './utils/index.js'
 export * from './types.js'
-
-export * from './models/index.js'
+export * from '@commercetools/platform-sdk'
 
 export { MAX_ITEMS_PER_QUERY } from './constants.js'
